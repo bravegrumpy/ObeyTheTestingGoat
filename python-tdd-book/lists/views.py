504447aa9573fa94de250e416978289a3fd618ff
Robin Zollner <robin.zollner97@gmail.com>
2023-06-27 06:51:46 -0400

    adding naieve item implementation to views.

--- python-tdd-book/lists/views.py
+++ python-tdd-book/lists/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
+from lists.models import Item
+
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home_page(request):
+    item = Item()
+    # TODO: FIXME: This adds an empty entry for every desired entry.
+    item.text = request.POST.get('item_text', '')
+    item.save()
+
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+        'new_item_text': request.POST.get('item_text', '')
     })
