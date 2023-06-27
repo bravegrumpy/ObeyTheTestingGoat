6599bffd94fdd4633c07800ae25f5f1a064b6cf3
Robin Zollner <robin.zollner97@gmail.com>
2023-06-27 04:47:53 -0400

    Front page HTML now generated from template

--- python-tdd-book/functional_tests.py
+++ python-tdd-book/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
@@ -15,7 +16,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         """
         Edith has heard about a cool new online to-do app. She goes\
-to check out its homepage.
+ to check out its homepage.
         """
         self.browser.get("http://localhost:8000")
 
@@ -23,13 +24,13 @@ to check out its homepage.
         She notices the page title and header mention to-do lists
         """
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text
         self.assertIn('To-Do', header_text)
 
         """
         She is invited to enter a to-do item straight away
         """
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -48,14 +49,15 @@ is tying fly-fishing lures)
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item.  She
-        # enters "use peacock feathers to make a fly" (Edith is very 
+        # enters "use peacock feathers to make a fly" (Edith is very
         # methodical)
         self.fail('Finish the test!')
 
