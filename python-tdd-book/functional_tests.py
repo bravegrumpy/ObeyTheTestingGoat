c15c399f152a022b23a255ff93545478216d4a38
Robin Zollner <robin.zollner97@gmail.com>
2023-06-28 23:13:26 -0400

    changing docstring into standard comments.

--- python-tdd-book/functional_tests.py
+++ python-tdd-book/functional_tests.py
@@ -19,15 +19,12 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        """
-        Edith has heard about a cool new online to-do app. She goes to check\
- out its homepage.
-        """
+        # Edith has heard about a cool new online to-do app. She 
+        # goes to check out its homepage.
+        
         self.browser.get("http://localhost:8000")
-
-        """
-        She notices the page title and header mention to-do lists
-        """
+    
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
