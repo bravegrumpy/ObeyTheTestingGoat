1277ca665517fe24885e64323a92800e37605890
Robin Zollner <robin.zollner97@gmail.com>
2023-06-30 00:36:56 -0400

    adding scratchpad file to separate out todo's into their own file, while keeping them tracked.

---
+++ scratchpad.py
@@ -0,0 +1,5 @@
+# TODO (feature): #4 Support more than one list
+# TODO #7 (scratchpad): Adjust model so that items are associated with different lists.
+# TODO #8 (scratchpad): Add unique URLs for each list.
+# TODO #9 (scratchpad): Add a URL for creating a new list via POST
+# TODO #10 (scratchpad): Add URLs for adding a new item to an existing list via post.
