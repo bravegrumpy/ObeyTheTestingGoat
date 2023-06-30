77b777a33a20954df8f952e9e46bebe724f49885
Robin Zollner <robin.zollner97@gmail.com>
2023-06-29 22:21:00 -0400

    Changes to functional tests

--- python-tdd-book/functional_tests/tests.py
+++ python-tdd-book/functional_tests/tests.py
@@ -3,9 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-
+from selenium.common.exceptions import WebDriverException
 
 class NewVisitorTest(LiveServerTestCase):
+
+    MAX_WAIT = 10
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -13,10 +15,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """
@@ -25,26 +35,35 @@ class NewVisitorTest(LiveServerTestCase):
         """        
         self.browser.get(self.live_server_url)
     
-        # She notices the page title and header mention to-do lists
+        """
+        She notices the page title and header mention to-do lists.
+        """
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
 
-        # She is invited to enter a to-do item straight away
+        """
+        She is invited to enter a to-do item straight away.
+        """
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
-        # She types "By peacock feathers" into a text box (Edith's hobby is
-        # tying fly-fishing lures)
+        """
+        She types "Buy peacock feathers" into a text box. (Edith's hobby
+        is tying fly-fishing lures)
+        """
         inputbox.send_keys('Buy peacock feathers')
 
-        # When she hits enter, the page updates, and now the lists 
-        # "1: peacock feathers" as an item in a to-do list
+        """
+        When she hits enter, the page updates, and now the page lists\
+            "1: peacock feathers" as an item in a to-do list.
+        """
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is stilla text box inviting her to add another item.
