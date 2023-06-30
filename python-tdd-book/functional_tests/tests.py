ec3b71a2e022ec2e3ef8b257aa8ecedcff258a3d
Robin Zollner <robin.zollner97@gmail.com>
2023-06-29 23:29:43 -0400

    Converting comments back into docstring format

--- python-tdd-book/functional_tests/tests.py
+++ python-tdd-book/functional_tests/tests.py
@@ -6,6 +6,13 @@ import time
 from selenium.common.exceptions import WebDriverException
 
 class NewVisitorTest(LiveServerTestCase):
+    """
+    This is my functional Test class
+
+    :param LiveServerTestCase: Django provided test server
+    :type LiveServerTestCase: test server
+    :raises e: Assertion Errors
+    """
 
     MAX_WAIT = 10
 
@@ -66,15 +73,19 @@ class NewVisitorTest(LiveServerTestCase):
         
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        # There is stilla text box inviting her to add another item.
-        # She enters "use peacock feathers to make a fly" (Edith
-        # is very methodical.)
+        """
+        There is still a text box inviting her to add another item.
+            She enters "use peacock feathers to make a fly" (Edith
+            is very methodial.)
+        """
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        # The page updates again, and now shows both items on her list.
+        """
+        The page updates again, and now shows both items on her list.
+        """
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table(
             '2: Use peacock feathers to make a fly'
